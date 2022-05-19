8a3c8ae51f948df7c537b27ad4d614b499b5ffa8
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-19 17:08:27 -0500

    Feat(app.py): pycodestyle was corrected

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.errorhandler(404) 
+
+@app.errorhandler(404)
 def error(e):
     """Handler for 404 errors"""
     return jsonify({"error": "Not found"}), 404
+
 
 @app.teardown_appcontext
 def teardown(self):
