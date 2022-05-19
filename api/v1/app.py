9d63ea0c332a776f526a2a6787d2b32d0daf5d20
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-19 17:06:55 -0500

    Feat(app.py): handler for 404 errors was added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,21 @@
 #!/user/bin/python3
 """This script starts a Flask web application"""
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404) 
+def error(e):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 @app.teardown_appcontext
 def teardown(self):
+    """Removes the current SQLAlchemy Session"""
     return storage.close()
 
 
