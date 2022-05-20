6afa66e7a2f1bdde6efe181a518fb8c6069bcf7a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-20 15:21:31 -0500

    Feat(app.py): typo was corrected

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,5 +23,5 @@ def error(e):
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_HOST") else 5000
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
