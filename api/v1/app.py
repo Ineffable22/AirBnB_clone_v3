ffec58cf2837092f4eb554b20eb72f1c01367678
MiguelBarreraDev <barreram987@gmail.com>
2022-05-21 15:21:25 -0500

    Add: CORS setup for my Flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""This script starts a Flask web application"""
+"""
+    This script starts a Flask web application
+"""
 from os import getenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origin": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
