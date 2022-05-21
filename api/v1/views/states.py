c15b16249eb8b9b106179362b7943df1952c6a2e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-20 22:10:13 -0500

    Feat(states.py): code was updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,6 @@
 all default RESTFul API actions"""
 from models import storage
 from models.state import State
-from models.base_model import BaseModel
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 
