6a004c2d8fb48de273781258eee43e959b14414c
MiguelBarreraDev <barreram987@gmail.com>
2022-05-21 19:26:56 -0500

    Update: Refactor code

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -64,11 +64,11 @@ def insert_place(city_id):
     return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route("/api/v1/places_search", methods=["POST"],
+@app_views.route("/places_search", methods=["POST"],
                  strict_slashes=False)
 def places_search():
     """Retrieves all Place objects depending of the body of the request"""
-    print("volvimos peraaa")
+    print("volvimos perraa")
     body = request.get_json()
     id_states = body.get("states") if body.get("states") else []
     id_cities = body.get("cities") if body.get("cities") else []
@@ -88,13 +88,12 @@ def places_search():
         states = [storage.get(State, _id) for _id in id_states]
         cities = [city for state in states for city in state.cities]
         places = [place for city in cities for place in city.places]
-        # places = [place for _id in id_states for city in storage.get(State, _id).cities for place in city.places]
     elif id_cities != []:
         cities = [storage.get(City, _id) for _id in id_cities]
         places = [place for city in cities for place in city.places]
     if id_amenities != []:
         places = list(filter(lambda e: e.amenities in id_amenities, places))
-    return jsonify([place.to_dict() for place in places])
+    return jsonify([place.to_dict() for place in places], )
 
 
 @app_views.route("/places/<place_id>", methods=["PUT"],
