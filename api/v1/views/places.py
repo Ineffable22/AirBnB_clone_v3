a0ccbd91a2f7e6ccae6f677d7e6bb4db666f42ad
MiguelBarreraDev <barreram987@gmail.com>
2022-05-21 19:00:26 -0500

    Add: New endpoint POST /api/v1/places_search that retrieves all Place objects depending of the body of the request

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.state import State
 
 
 @app_views.route("/cities/<city_id>/places", methods=["GET"],
@@ -61,6 +62,39 @@ def insert_place(city_id):
     new_place.city_id = city_id
     new_place.save()
     return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route("/api/v1/places_search", methods=["POST"],
+                 strict_slashes=False)
+def places_search():
+    """Retrieves all Place objects depending of the body of the request"""
+    print("volvimos peraaa")
+    body = request.get_json()
+    id_states = body.get("states") if body.get("states") else []
+    id_cities = body.get("cities") if body.get("cities") else []
+    id_amenities = body.get("amenities") if body.get("amenities") else []
+    places = []
+    if type(body) != dict:
+        abort(400, description="Not a JSON")
+    if body == {} or len(id_states + id_cities + id_amenities) == 0:
+        places = storage.all(Place).values()
+    elif id_states != [] and id_cities != []:
+        states = [storage.get(State, _id) for _id in id_states]
+        cities = [city for state in states for city in state.cities]
+        cities += [storage.get(City, _id) for _id in id_cities]
+        cities = list(set(cities))
+        places = [place for city in cities for place in city.places]
+    elif id_states != [] and id_cities == []:
+        states = [storage.get(State, _id) for _id in id_states]
+        cities = [city for state in states for city in state.cities]
+        places = [place for city in cities for place in city.places]
+        # places = [place for _id in id_states for city in storage.get(State, _id).cities for place in city.places]
+    elif id_cities != []:
+        cities = [storage.get(City, _id) for _id in id_cities]
+        places = [place for city in cities for place in city.places]
+    if id_amenities != []:
+        places = list(filter(lambda e: e.amenities in id_amenities, places))
+    return jsonify([place.to_dict() for place in places])
 
 
 @app_views.route("/places/<place_id>", methods=["PUT"],
